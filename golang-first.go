d56c00b9af2b1114b276a1e385fa688edc17651c
arvardy184 <arvanardana1@gmail.com>
2023-02-14 16:23:54 +0700

    update golang-first

--- golang-first.go
+++ golang-first.go
@@ -1,5 +1,5 @@
 package golangfirst
 
-func SayHello() string{
-return "Hellooo"
+func SayHello() string {
+	return "Hellooo Arvan kamu keren banget udah bisa bikin github update modules dan depedencies keren keren semoga kamu lolos bcc yaa"
 }
